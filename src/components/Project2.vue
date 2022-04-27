1f5cdf78aeb9c38c7157f3fe5e2acf5bfae85204
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-27 09:31:43 -0600

    projects

---
+++ src/components/Project2.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="project">
+    <img src="../assets/project2-photo.png" alt="Project2-photo">
+    <div>
+      <h1><a href="">PROJECT NAME</a></h1>
+      <p>About this project</p>
+      <div class="project-resource">
+        <h4><a href="">Deployed Site</a></h4>
+        <p><a href="">GitHub Repository</a></p>
+      </div>
+    </div>
+    <div class="tech-used">
+      <h2>Technology Used</h2>
+      <div class="icons">
+        <img src="../assets/p2tech1-icon.png" alt="p2Tech1">
+        <img src="../assets/p2tech2-icon.png" alt="p2Tech2">
+        <img src="../assets/p2tech3-icon.png" alt="p2Tech3">
+        <img src="../assets/p2tech4-icon.png" alt="p2Tech4">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Project2'
+  }
+</script>
+
+<style>
+</style>
