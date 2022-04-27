cad9892315735983f8ed2dc8f883b1e1b9ae2c9a
Jenna Leopold <jleopold424@gmail.com>
2022-04-27 11:34:23 -0400

    created ContactInfo component

---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="profile-photo-wrapper">
+      <img src="../assets/profile-photo.png" alt="profile photo" />
+    </div>
+     <div class="contact-info-wrapper">
+       <h2>CONTACT INFO</h2>
+       <p>Name</p>
+       <p><a href="">Email</a></p>
+       <p><a href="">LinkedIn</a></p>
+       <p><a href="">GitHub</a></p>
+      <p><a href="">Resume</a></p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ContactInfo'
+  }
+</script>
+
+<style>
+</style>
