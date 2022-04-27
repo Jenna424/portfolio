b066a676d4588fcec7484207f8b4a9c16f7894b0
Chinwendu <chinwenduukoha@gmail.com>
2022-04-27 11:32:06 -0400

    home page init

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <div class="profile-image">
+      <img src="./assets/image.jpeg" alt="profile-image" />
+    </div>
+    <div class="introduction">
+      <h3>Hello there!</h3>
+      <p>I am a full-stack web developer</p>
+    </div>
   </div>
 </template>
 
