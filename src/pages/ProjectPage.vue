3215fab24a07b177844719b44169f4764709c266
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-30 12:14:29 -0600

    resumePage

---
+++ src/pages/ProjectPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="introduction">
+      <h3>Projects</h3>
+      <p>A continuously updated project site. Click to view more details</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProjectPage'
+  }
+</script>
+
+<style>
+</style>
